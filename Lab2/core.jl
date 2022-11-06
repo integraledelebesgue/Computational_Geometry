6499ff465f148c483a9a05ecf67b5add742dc071
integraledelebesgue <jsmolka@student.agh.edu.pl>
2022-11-06 11:14:33 +0100

    ...

--- Lab2/core.jl
+++ Lab2/core.jl
@@ -3,8 +3,8 @@ push!(LOAD_PATH, @__DIR__)
 using generators 
 using Plots
 
-n = 200
+n = 10000
 
-points = square_diagonals(n, 10.0)
+#points = circle(n, 10.0)
 
-plot(points[1, 1:n], points[2, 1:n], seriestype = :scatter, size=(500, 500))
+#plot(points[1, 1:n], points[2, 1:n], seriestype = :scatter, size=(500, 500))
